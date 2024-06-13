cd584661bf4fd4bb4773a8e6e6d913af3213dd4b
RyanAndersonG64 <ryan.anderson.g64@gmail.com>
2024-06-13 16:42:01 -0400

    i swear if this fixes it

--- src/Forum.jsx
+++ src/Forum.jsx
@@ -15,7 +15,7 @@ const Forum = () => {
     const storedUser = JSON.parse(localStorage.getItem('storedUser'))
 
     const [allPosts, setAllPosts] = useState([])
-    const {postState, setPostState} = useState([])
+    const [postState, setPostState] = useState([])
 
     const [title, setTitle] = useState('')
     const [textContent, setTextContent] = useState('')
