0dfe42c9c69aa9570b058ec33413fc8866087b86
RyanAndersonG64 <ryan.anderson.g64@gmail.com>
2024-06-13 13:25:04 -0400

    rankings page should be functional

--- src/Rankings.jsx
+++ src/Rankings.jsx
@@ -1,6 +1,34 @@
+import { fetchCoasters, fetchUser, fetchParks } from "./api"
+import { useContext, useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
 
+import { AuthContext } from "./context"
+import { UserContext } from "./usercontext"
+import { ParkContext } from "./parkcontext"
+import { CoasterContext } from "./coasterContext"
+
+import GetUser from "./getUser"
+import LocationSelector from "./LocationSelector"
 
 const Rankings = () => {
+
+    const { auth } = useContext(AuthContext)
+
+    const authStorage = localStorage.getItem('authStorage')
+    const storedUser = JSON.parse(localStorage.getItem('storedUser'))
+
+
+    const navigate = useNavigate()
+
+  useEffect (
+        () => {
+          
+              auth.setAccessToken(authStorage)
+              setCurrentUser(storedUser)
+
+        },
+        []
+      )
 
     return (
         <div>
