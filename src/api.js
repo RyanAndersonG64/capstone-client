5a4aeeaf28e7db422ff3a737cf0df879adcfcbc5
RyanAndersonG64 <ryan.anderson.g64@gmail.com>
2024-06-13 12:23:22 -0400

    pls 2

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // const baseUrl = "http://127.0.0.1:8000"
-const baseUrl = 'https://ryan-anderson-capstone-server-2.fly.dev/'
+const baseUrl = 'https://ryan-anderson-capstone-server-2.fly.dev'
 
 // -- Authentication -- 
 
