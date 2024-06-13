ff29d9ab5f21a8820111b479834c0b9e952acdbf
RyanAndersonG64 <ryan.anderson.g64@gmail.com>
2024-06-13 15:17:52 -0400

    turbocringe

--- src/Forum.jsx
+++ src/Forum.jsx
@@ -15,7 +15,7 @@ const Forum = () => {
     const storedUser = JSON.parse(localStorage.getItem('storedUser'))
 
     const [allPosts, setAllPosts] = useState([])
-    const {postState, setPostState} = useContext([])
+    const {postState, setPostState} = useContext(postState)
 
     const [title, setTitle] = useState('')
     const [textContent, setTextContent] = useState('')
