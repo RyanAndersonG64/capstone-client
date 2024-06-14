6c64b5fcdd775ad432e345fb77029ef78526bdd9
RyanAndersonG64 <ryan.anderson.g64@gmail.com>
2024-06-14 08:44:57 -0400

    bruh

--- src/main.jsx
+++ src/main.jsx
@@ -34,7 +34,7 @@ import PersonalRanking from './PersonalRanking'
 import Rankings from './Rankings'
 import Forum from './Forum'
 import ImageGallery from './ImageGallery'
-import Social from './social'
+import Social from './Social'
 
 function Layout() {
   return (
